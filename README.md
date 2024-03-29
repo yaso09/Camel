f95697b837df5b7d42b599eac23f6ffd8562ae1a
Yasir Eymen Kayabaşı <93262823+yaso09@users.noreply.github.com>
2024-03-29 18:44:25 +0300

    send to github

--- README.md
+++ README.md
@@ -1,5 +1,60 @@
 # Camel
 > Script manager
 
+## Installation
+For Windows use WSL
+Need Python 3.10.x
+```bash
+git clone https://github.com/yaso09/camel.git
+cd camel
+```
+Build code
+```bash
+sh build.sh
+```
+Now you can remove clone
+```bash
+cd ..
+rm -rf camel
+```
 
+## Example Usage
+Create example script
+```bash
+echo "echo Hello World!">>test.sh
+```
+Package this script
+```bash
+camel -b
+```
+Answer questions
+```
+Script file (please use formats like `main.py`) (script.sh): test.sh
+Script name (your folder name): test
+Command (write `exit` to stop): sh test.sh
+>> exit
+Your script is packaging...
+{'name': 'test', 'file_format': 'sh', 'commands': ['sh test.sh'], 'codes': ['echo Hello World!']}
+Is this OK? (yes): yes
+```
+Test script
+```bash
+camel -t test
+```
+Script will return `Hello World!`.
+
+## Repository Config
+For use another repositories open config page
+```bash
+camel -c
+```
+It will returns:
+```
+CONFIGURE CAMEL
+
+Repository: https://github.com/yaso09/camel
+
+Do you want to change repository? (no): yes
+Enter Github repository (user/repo): YOUR GITHUB REPO
+```
 
--- main.py
+++ main.py
@@ -1,46 +1,79 @@
+# Imports
+
 import os, sys, json, uuid
 from requests import get as g
 from requests import post as p
+
+# Print information text
 
 print("""
 Camel - Script Manager 1.0.0
 Type `-h` or `help` for more information
 """)
 
+# Set configuration
+
+global cfg
+
+fpath = os.path.join(os.path.expanduser("~"), "bin", "camel_config.json")
+if os.path.exists(fpath):
+    f = open(fpath, "r")
+    try:
+        j = json.load(f)
+        repo = j["repository"]
+    except: repo = "yaso09/camel"
+else:
+    f = open(fpath, "w")
+    c = {
+        "repository": "yaso09/camel"
+    }
+    json.dump(c, f)
+    repo = c["repository"]
+
+# Get parameter
+
 if not len(sys.argv) > 1: os._exit(0)
 
 param = sys.argv[1]
+
+# Define run command
 
 if param == "run" or param == "-r":
     try:
         nm = sys.argv[2]
         res = g(
-            f"https://raw.githubusercontent.com/yaso09/camel/scripts/{nm}.cml"
+            f"https://raw.githubusercontent.com/{repo}/scripts/{nm}.cml"
         )
         
         if res:
             nm = uuid.uuid4().hex
             f = open(f"script_{nm}.json", "w")
-            f.write(res.text) and f.close()
+            json.dump(res.text, f) and f.close()
             a = open(f"script_{nm}.json", "r")
             script = json.load(a)
-            f = open(f"script_{nm}.{script['file_format']}", "w")
+            f = open(f"{script['name']}.{script['file_format']}", "w")
             f.writelines(script["codes"])
             commands = script["commands"]
             for command in commands:
                 os.system("commands")
         else: print("Script not found")
     except IndexError: print("Please enter script name")
+
+# Define list command
+
 elif param == "list" or param == "-l":
-    url = "https://api.github.com/repos/yaso09/camel/contents/scripts"
+    url = f"https://api.github.com/repos/{repo}/contents/scripts"
     res = g(url)
     files = res.json()
     print("Script Name")
     for file in files:
         print(str(file["name"].split(".cml")[0]))
+
+# Define build command
+
 elif param == "build" or param == "-b":
     commands = []
-    file = input("Script file please use formats like `main.py` (script.sh): ")
+    file = input("Script file (please use formats like `main.py`) (script.sh): ")
     name = input(f"Script name ({os.getcwd().split('/')[len(os.getcwd().split('/'))-1]}): ")
     command = input("Command (write `exit` for stop): ")
     while not command == "exit":
@@ -57,6 +90,9 @@ elif param == "build" or param == "-b":
     codes = f.readlines()
     while "\n" in codes:
         codes.remove("\n")
+    for code in codes:
+        if "\n" in code:
+            codes[codes.index(code)] = codes[codes.index(code)].split("\n")[0]
     build = {
         "name": name,
         "file_format": file.split(".")[len(file.split(".")) - 1],
@@ -69,38 +105,86 @@ elif param == "build" or param == "-b":
     if not ok == "yes": print("Packaging cancelled")
     else:
         f = open(f"{name}.cml", "w")
-        f.write(str(build))
+        json.dump(build, f)
         f.close()
         print(f"Your script packaged to `{name}.cml`")
+
+# Define save command
+
 elif param == "save" or param == "-s":
     try:
         nm = sys.argv[2]
         try: os.system(f'alias {sys.argv[3]}="$HOME/bin/camel run {nm}"')
         except IndexError: os.system(f'alias {nm}="$HOME/bin/camel run {nm}"')
     except IndexError: print("Please enter script name")
+
+# Define test command
+
 elif param == "test" or param == "-t":
     try:
         nm = uuid.uuid4().hex
-        a = open(f"{sys.argv[2]}.cml", "r")
-        script = json.load(a)
-        f = open(f"script_{nm}.{script['file_format']}", "w")
-        f.writelines(script["codes"]) and f.close()
-        commands = script["commands"]
-        for command in commands:
-            os.system("commands")
+        try:
+            os.system(f"cp {sys.argv[2]}.cml {sys.argv[2]}.json")
+            a = open(f"{sys.argv[2]}.json", "r")
+            script = json.load(a)
+            f = open(f"{script['name']}.{script['file_format']}", "w")
+            f.writelines(script["codes"]) and f.close()
+            commands = script["commands"]
+            for command in commands:
+                os.system(str(command))
+            os.remove(f"{sys.argv[2]}.json")
+            os.remove(f"{script['name']}.{script['file_format']}")
+        except FileNotFoundError: print("Script not found")
+        except json.JSONDecodeError:
+            print("Can't decode script")
+            os.remove(f"{sys.argv[2]}.json")
     except IndexError: print("Please enter script name")
+
+# Define config command
+
+elif param == "config" or param == "-c":
+    print(f"""
+CONFIGURE CAMEL
+
+Repository: https://github.com/{repo}
+""")
+    ok = input("Do you want to change repository? (no): ")
+    if not ok: ok = "no"
+    if not ok == "no":
+        new_repo = input("Enter Github repository (user/repo): ")
+        f = open(fpath, "w")
+        c = {
+            "repository": new_repo
+        }
+        json.dump(c, f)
+
+# Define update command
+
+elif param == "update" or param == "-u":
+    b = g("https://raw.githuusercontent.com/yaso09/camel/main/build.sh")
+    cml = g("https://raw.githuusercontent.com/yaso09/camel/main/main.py")
+    r = os.getcwd() and os.system("cd $HOME")
+    os.mkdir("tmp") and os.system("cd tmp")
+    f = open("main.py", "w") and f.write(cml.text) and f.close()
+    os.system(b.text) and os.remove("main.py") and os.system(f"cd {r}")
+
+# Define help command
+
 elif param == "help" or param == "-h":
-    print("""
+    print(f"""
 COMMAND\t\tUSAGE
 
 build  (-b)\tBuild script to Camel package
+config (-c)\tConfigure Github repository
 help   (-h)\tGet help
 list   (-l)\tList online scripts
 run    (-r)\tRun script
 save   (-s)\tSave alias of script
 test   (-t)\tTest script in local
+update (-u)\tUpdate CLI
 
-Github: https://github.com/yaso09/Camel
+Repository: https://github.com/{repo}
+Source Code: https://github.com/yaso09/Camel
 """)
 
 # This monolitic CLI created by Yasir Eymen KAYABAŞI
